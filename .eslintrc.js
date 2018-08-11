ea7cc392275789fc2808c3de47e574dcc4ab9028
毛贤德 <xiande.mao@hex-tech.net>
2018-08-11 19:29:00 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    env: {
+      browser: true,
+    },
+    extends: [
+      'react-app',
+      // https://github.com/standard/standard/blob/master/docs/README-zhcn.md
+      'standard'
+    ],
+    plugins: [
+      //'react'
+    ],
+    // add your custom rules here
+    rules: {
+      // allow async-await
+      'generator-star-spacing': 'off',
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-console': 'error',
+      //'eqeqeq':'warn'
+    }
+  }
+  
