a459710d2390907d0adf64a303cb975deb915d19
毛贤德 <394097441@qq.com>
2018-08-12 23:23:47 +0800

    add log4web

--- service/api/demo.js
+++ service/api/demo.js
@@ -1,8 +1,17 @@
-import { Register } from 'apibus'
+import log4web from 'log4web'
+import { Register, api } from 'apibus'
 
-@Register('demo')
+let log = log4web('api.demo')
+
+@Register('Demo')
 export class Demo {
   test () {
-    alert('demo')
+    log.debug('demo debug')
+    log.info('demo info')
+    log.warn('demo warn')
+    log.error('demo error')
+    log.fatal('demo fatal')
   }
 }
+
+api.Demo.test()
---
+++ service/config/index.js
@@ -0,0 +1,7 @@
+import log4web from 'log4web'
+
+log4web.config({
+  // debug (args) {
+  //   alert(this.tag + ' + ' + args)
+  // }
+})
